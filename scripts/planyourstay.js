4ea8898858dab9352208284894dcb0154e959d81
unknown <svalente@atl.yearup.org>
2022-10-06 16:46:16 -0400

    index skeleton added and plan your stay html skeleton done 4:44pm

---
+++ scripts/planyourstay.js
@@ -0,0 +1,19 @@
+"use script"
+function estimateBtnClicked() {
+    const checkInDate = new Date(document.getElementById("checkInDate").value);
+    const numberOfNights = document.getElementById("#numberOfNights").value;
+
+    let priceForQueen = 
+
+
+
+
+    
+}
+
+
+
+/* if (inSeason && roomType == "king") {
+    price = 250 * numNights;
+  } else if {
+      */

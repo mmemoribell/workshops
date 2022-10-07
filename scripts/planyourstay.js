a21a1579f9534f34283a363298bad6a333cb7cda
unknown <svalente@atl.yearup.org>
2022-10-07 11:53:05 -0400

    js has the discount added no calculations in yet 11:52am 7/10/22

--- scripts/planyourstay.js
+++ scripts/planyourstay.js
@@ -2,18 +2,54 @@
 function estimateBtnClicked() {
     const checkInDate = new Date(document.getElementById("checkInDate").value);
     const numberOfNights = document.getElementById("#numberOfNights").value;
+    let roomSelection = document.querySelector("input[name='roomSelection']:checked");
+    let discounts = document.querySelector("input[name='discount']:checked");
 
-    let priceForQueen = 
-
-
-
-
+    let priceForQueenKing = 150 * numberOfNights;
+    let priceOfSuite = 210 * numberOfNights;
+    let price = 0;
     
+let currentMonth = checkInDate.getMonth();
+let inSeason = false
+switch (currentMonth) {
+    case 5:
+    case 6:
+    case 7:
+        inSeason = true;
+        break;
+        default:
+          inSeason = false;
+        break;
 }
 
+if (inSeason && (roomSelection == "queen" || roomSelection == "king" )) {
+    price = 250 * numberOfNights;
+} else if(!inSeason && (roomSelection == "queen" || roomSelection == "king")) {
+    price = 150 * numberOfNights;
+}
+if (inSeason && (roomSelection == "2bdSuite" )) {
+    price = 350 * numberOfNights;
+}
+  else if(!inSeason && (roomSelection == "2bdSuite")) {
+    price = 210 * numberOfNights;
+}
 
+if (discount == "AAA/Senior") {
+    price = .10 * roomCost;
+}
+if (discount == "military") {
+    price = .20 * roomCost;
+}
 
-/* if (inSeason && roomType == "king") {
-    price = 250 * numNights;
-  } else if {
-      */
+const taxes = 12% ;
+const roomCost = document.getElementsById("#roomCost").innertext;
+}
+//input price values on each room -
+//make a value of 250$ for all rooms if check in dat is durin jun-aug-
+//max capaciry for queen is 5, king 2, 2bdsuite 6.
+//which will give a message of max capacity
+//implement a function of getroomrate()
+//add discount aa/senior 10%,military is 20%
+//conformation number
+
+ 
